f75358692f8d69f83b2566780f153b51e8a09810
Nduchin <nduchin@mail.ru>
2023-05-05 16:39:54 +0300

    feat(02): complete task

---
+++ 02-write-file/index.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const fs = require('fs');
+
+const { stdin, stdout, exit } = process;
+
+const filePath = path.join(__dirname, 'text.txt');
+const writeStream = fs.createWriteStream(filePath);
+
+function appendFile(data) {
+  if (/exit/.test(data)) {exit(); }
+  writeStream.write(data);
+}
+
+stdout.write('Input text\n');
+stdin.on('data', appendFile);
+
+process.on('exit', () => stdout.write('exiting programm\n'));
+process.on('SIGINT', () => { stdout.write('force '); exit(); });
