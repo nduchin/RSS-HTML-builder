d588179b41f99f3b43c3e192761bd29eb7a950b6
Nduchin <nduchin@mail.ru>
2023-05-06 07:28:42 +0300

    chore: update eslint, add task data files to .eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,15 @@
 module.exports = {
-  'env': {
-    'commonjs': true,
-    'es2022': true,
-    'node': true
-  },
-  'extends': 'eslint:recommended',
-  'parserOptions': {
-    'ecmaVersion': 12
-  },
-  'rules': {
-    'indent': [
-      'error',
-      2
+    "env": {
+        "commonjs": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
     ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'always'
-    ]
-  }
-};
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+    }
+}
