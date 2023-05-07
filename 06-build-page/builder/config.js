b949624ab6d993f33cbe8e491b444a64cea12a26
Nduchin <nduchin@mail.ru>
2023-05-07 03:57:35 +0300

    feat(06): add loader module, extract config to module

---
+++ 06-build-page/builder/config.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+const config = {
+  output: path.join(__dirname, 'dist'),
+}
+
+module.exports = config;
--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,30 +1,22 @@
 const path = require('path');
-const {mkdir} = require('fs/promises');
+const fs = require('fs');
+const config = require(path.join(__dirname, 'builder', 'config.js'))
 
 // TODO: main package-manager
 /**
  * builder main function
  * @param {Object} config object
  */
-function builder({ output = __dirname}) {
-  mkdir(output).then(()=>{console.log(`create dir`)}).catch((err)=> {
+async function builder({ output = __dirname,/* plugins = [] , clean = false*/}) {
+  await fs.promises.mkdir(output).then(()=>{console.log('create dir')}).catch((err)=> {
     if (err.code === 'EEXIST') {
-      console.log(`dir already exists`)
+      console.log('dir already exists');
     } else {
-      throw err
+      throw err;
     }
   });
-  // mkdir 
   // plugins
   // done
-}
-
-// TODO: html-plugin
-// TODO: css bundler-plugin
-// TODO: assets copy-plugin
-
-const config = {
-  output: path.join(__dirname, 'dist'),
 }
 
 builder(config)
