904de8896751b17f4f43e063ea0b740225d8ca9e
Nduchin <nduchin@mail.ru>
2023-05-08 12:54:44 +0300

    fix(03): define search method, output messages

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,17 +1,33 @@
 const path = require('path');
 const fs = require('fs');
 
-async function getStats(files) {
-  let res = await Promise.all(files.map((file) => {
-    return fs.promises.stat(path.join(folder, file)).then((stat) => {
-      return stat.size ? [file, path.extname(file).slice(1), stat.size] : null;
-    });
+async function getStats(dirents) {
+  let res = await Promise.all(dirents.filter(dirent => dirent.isFile()).map(async(file) => {
+    return [file.name, (await fs.promises.stat(path.join(folder, file.name))).size];
   }));
   return res.filter((elem) => !!elem);
 }
 
 function logFiles(files) {
-  console.debug(files.sort(fileSort));
+  files.sort(fileSort).forEach((file) => {
+    process.stdout.write(`${
+      fixedWidth(path.parse(file[0]).name, 7)
+    }-  ${
+      fixedWidth(path.extname(file[0]).slice(1), 6)
+    }-  ${
+      fixedWidth((Math.floor(file[1]/102.4)/10).toString(),5)
+    }KB (${
+      fixedWidth(file[1].toString(), 6)
+    }B)\n`)
+  })
+}
+
+function fixedWidth(str, width) {
+  if (str.length >= width) {
+    return str.slice(0, width);
+  } else {
+    return str+' '.repeat(width - str.length)
+  }
 }
 
 function fileSort(a, b) {
@@ -23,4 +39,4 @@ function fileSort(a, b) {
 }
 
 const folder = path.join(__dirname, 'secret-folder');
-fs.promises.readdir(folder).then(getStats).then(logFiles);
+fs.promises.readdir(folder, {withFileTypes: true}).then(getStats).then(logFiles);
